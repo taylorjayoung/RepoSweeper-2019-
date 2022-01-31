a4e6f9dddc50cdbb2a6b343d38e16513e4a5f13e
Taylor Young <taylorjayoung@google.com>
2022-01-30 22:56:02 -0500

    temp

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import Popup from 'react-popup';
 
 ReactDOM.render(
   <>
-    <App />
+    {/* <App /> */}
+  <div className="maintenance">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Source+Serif+4:wght@200&display=swap');
+    </style>
+
+    <h1 className="maintenance_message">Sorry we are down for maintenance!</h1>
+  </div>
+
   < />
   , document.getElementById('root'));
 
